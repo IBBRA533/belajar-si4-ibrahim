8bd3e4d322d4174afaa369244c78f2aef1461462
IBBRA533 <ibraputra38@gmail.com>
2025-09-24 14:06:47 +0700

    belajar1

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/profil', function () {
+    return view('profil');
+});
